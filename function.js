e760406b0edeee98987689e877bc17e551f3871d
Vinz <vincentballut@outlook.fr>
2019-09-30 09:02:18 +0200

    create default parameters

--- function.js
+++ function.js
@@ -8,4 +8,26 @@ var addString = function (a, b) {
     return a + b;
 };
 log(addString("coucou ", "toi"));
-log(addString("coucou toi"));
+log(addString("coucou toi "));
+//give a defaut parameter
+function buildName(firstName, lastName) {
+    if (lastName === void 0) { lastName = "Default name"; }
+    return firstName + " " + lastName;
+}
+var myName = buildName("Name");
+var myOtherName = buildName("Bruce", "Banner");
+log(myName);
+log(myOtherName);
+function addTwoNumner(numberOne, numberTwo) {
+    if (numberTwo === void 0) { numberTwo = 5; }
+    return numberOne + numberTwo;
+}
+log(addTwoNumner(1));
+function buildPerson(firstName) {
+    var remainingName = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        remainingName[_i - 1] = arguments[_i];
+    }
+    return firstName + " " + remainingName.join(" ");
+}
+log(buildPerson("Vinz", "et", "puis", "c'est", "tout"));
--- function.ts
+++ function.ts
@@ -12,3 +12,27 @@ const addString = (a: string, b?: string) => {
 
 log(addString("coucou ", "toi"));
 log(addString("coucou toi "));
+
+//give a defaut parameter
+function buildName(
+  firstName: string,
+  lastName: string = "Default name"
+): string {
+  return `${firstName} ${lastName}`;
+}
+
+const myName = buildName("Name");
+const myOtherName = buildName("Bruce", "Banner");
+log(myName);
+log(myOtherName);
+
+function addTwoNumner(numberOne: number, numberTwo: number = 5): number {
+  return numberOne + numberTwo;
+}
+log(addTwoNumner(1));
+
+function buildPerson(firstName: string, ...remainingName: string[]) {
+  return `${firstName} ${remainingName.join(" ")}`;
+}
+
+log(buildPerson("Vinz", "et", "puis", "c'est", "tout"));
